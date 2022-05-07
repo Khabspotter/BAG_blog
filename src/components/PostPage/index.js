01b4e98cc1136ea9340dc61524e6263226a967fa
unknown <max96rusel@mail.ru>
2022-05-07 18:16:48 +0500

    Добавляет темную тему..

--- src/components/PostPage/index.js
+++ src/components/PostPage/index.js
@@ -3,7 +3,6 @@ import { useParams, useNavigate } from "react-router-dom";
 import api from "../../utils/api";
 import dayjs from "dayjs";
 import "./index.css";
-import TextField from "@mui/material/TextField";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 import PostContext from "../Contexts/postContext";
 import IconButton from "@mui/material/IconButton";
@@ -12,29 +11,29 @@ import UserContext from "../Contexts/userContext";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
 export const PostPage = () => {
-  const { userInfo, setUserInfo } = useContext(UserContext)
-  const { posts, setPosts } = useContext(PostContext)
+  const { userInfo, setUserInfo } = useContext(UserContext);
+  const { posts, setPosts } = useContext(PostContext);
   const [post, setPost] = useState(null);
   const [comments, setComments] = useState(null);
   const params = useParams();
   const navigate = useNavigate();
-  
-
-
+  const { writeLS } = useLocalStorage();
 
   const deletePost = () => {
     api
       .deletePosts(params.postID)
       .then((res) => {
         setPosts((prevState) => {
-          return prevState.filter((item) => item._id !== params.postID)
-        })
-        navigate('/')
+          return prevState.filter((item) => item._id !== params.postID);
+        });
+        navigate("/");
       })
       .catch((err) => alert(err));
   };
 
-
+  const addToFavorite = () => {
+    writeLS("favorites", params.itemID);
+  };
 
   useEffect(() => {
     api
@@ -65,56 +64,75 @@ export const PostPage = () => {
         params.postID
       )
       .then((data) => api.getComments(params.postID))
-      .then((data) => { setComments(data); event.target.new_comment.value = '' })
+      .then((data) => {
+        setComments(data);
+        event.target.new_comment.value = "";
+      })
       .catch((err) => alert("Заполните поле комментария"));
   };
 
   return (
-    <div className="page">
-      <div className="post">
-        <h1>{post?.title}</h1>
-        <p className="date">
-          {dayjs(post?.created_at).format("DD.MM.YYYY, HH:mm:ss")}
-        </p>
-        <div className="author">{post?.author.name}</div>
-        <img src={post?.image} />
-        <p>{post?.text}</p>
-      </div>
-      <div className="comments">
-        {comments?.map((el) => (
-          <div key={el._id}>
-            <div style={{ display: "flex" }}>
-              <img className="avatar" src={`${el.author.avatar}`} />
+    <div >
+     <button className="back" onClick={()=>navigate('/')}>Назад</button>
+    <div className="postpage">
+      <div className="page">
+        <div className="post">
+          <div className="title">{post?.title}</div>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div className="author">
+              <img className="avatar" src={`${post?.author.avatar}`} />
               <div>
-                <div className="author">{el.author.name}</div>
-                <div className="date">
-                  {dayjs(el.created_at).format("DD.MM.YYYY, HH:mm:ss")}
+                <p className="author_name">{post?.author.name}</p>
+                <p className="date">
+                  {dayjs(post?.created_at).format("DD.MM.YYYY, HH:mm:ss")}
+                </p>
+              </div>
+
+            </div>
+            <div>
+                {userInfo._id == post?.author._id && (
+                  <IconButton onClick={deletePost} title='Удалить пост'>
+                    <DeleteOutlinedIcon />
+                  </IconButton>
+                )}
+              </div>
+          </div>
+          <div>
+            <img className="image" src={post?.image} />
+          </div>
+          <div className="post_text">{post?.text}</div>
+        </div>
+        <div className="comments">
+          {comments?.map((el) => (
+            <div key={el._id}>
+              <div className="author">
+                <img className="avatar" src={`${el.author.avatar}`} />
+                <div>
+                  <div className="author_name">{el.author.name}</div>
+                  <div className="date">
+                    {dayjs(el.created_at).format("DD.MM.YYYY, HH:mm:ss")}
+                  </div>
                 </div>
               </div>
+              <div className="post_text">{el.text}</div>
+              <hr />
             </div>
-            <p> {el.text}</p>
-            <hr />
+          ))}
+          <div>
+            <form
+              style={{ display: "flex", flexDirection: "column", alignItems:'center'}}
+              onSubmit={handleSubmit}
+            >
+              <input
+                name="new_comment"
+                placeholder="Введите комментарий"
+              />
+              <button>Опубликовать</button>
+            </form>
           </div>
-        ))}
-        <div>
-          <form
-            style={{ display: "flex", flexDirection: "column" }}
-            onSubmit={handleSubmit}
-          >
-            <TextField
-              id="outlined-basic"
-              variant="outlined"
-              name="new_comment"
-              placeholder="Add your comment"
-            />
-            <button>Опубликовать</button>
-            {(userInfo._id == post?.author._id) && (<IconButton onClick={deletePost}>
-              <DeleteOutlinedIcon />
-            </IconButton>)}
-              
-          </form>
         </div>
       </div>
+    </div>
     </div>
   );
 };
